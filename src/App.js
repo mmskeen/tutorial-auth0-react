d8c4dbb33e9ca10d300bda8b38b031f209460fbd
Michael Skeen <mmskeen@gmail.com>
2020-03-31 14:24:59 -0600

    completed login example with auth0

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Router } from "@reach/router";
+import { useAuth } from "react-use-auth";
+import Callback from "./components/Callback";
 import './App.css';
 
 function App() {
+  const s = useAuth();
+
+  const Login = () => {
+    if (s.isAuthenticated()) {
+      console.log("user is: ", s.user);
+      console.log("useAuth is: ", s);
+      return <button onClick={s.logout}>Logout</button>;
+    } else {
+      return <button onClick={s.login}>Login</button>;
+    }
+  };
+
+  const Greeting = () => {
+    if (s.isAuthenticated()) {
+      return <h1>Hi, {s.user.name}!</h1>;
+    } else {
+      return <h1>Hi there! Please login.</h1>
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Auth0 Example</h1>
+      <Greeting />
+      <Login />
+      <Router>
+        <Callback path="/auth0_callback" />
+      </Router>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { navigate } from "@reach/router";
+import { AuthProvider } from "react-use-auth";
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
+  <AuthProvider
+    navigate={navigate}
+    auth0_domain={process.env.REACT_APP_AUTH0_CLIENT_ID}
+    auth0_client_id={process.env.REACT_APP_AUTH0_CLIENT_ID}>
     <App />
+    </AuthProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
