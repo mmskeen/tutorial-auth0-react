15de18881ab4a2bb23f2455397f694fbb4f0aeba
Michael Skeen <mmskeen@gmail.com>
2020-04-01 12:18:36 -0600

    cleaning up code a bit

--- src/App.js
+++ src/App.js
@@ -5,25 +5,20 @@ import Callback from "./components/Callback";
 import './App.css';
 
 function App() {
-  const s = useAuth();
+  const { isAuthenticated, login, logout, user } = useAuth();
 
   const Login = () => {
-    if (s.isAuthenticated()) {
-      console.log("user is: ", s.user);
-      console.log("useAuth is: ", s);
-      return <button onClick={s.logout}>Logout</button>;
-    } else {
-      return <button onClick={s.login}>Login</button>;
-    }
+      return isAuthenticated() ? 
+        <button onClick={logout}>Logout</button> :
+        <button onClick={login}>Login</button>;
   };
 
   const Greeting = () => {
-    if (s.isAuthenticated()) {
-      return <h1>Hi, {s.user.name}!</h1>;
-    } else {
-      return <h1>Hi there! Please login.</h1>
-    }
+    return isAuthenticated() ?
+      <h1>Hi, {user.name}!</h1> :
+      <h1>Hi there! Please login.</h1>;
   }
+
   return (
     <div className="App">
       <h1>Auth0 Example</h1>
